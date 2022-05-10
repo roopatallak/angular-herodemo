a28ba8c77dd61292d144ef75e6040751ca813e05
roopatallak <103457469+roopatallak@users.noreply.github.com>
2022-05-10 15:30:10 -0400

    Hero Example from Angular tutorial

---
+++ mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from './hero'
+
+export const HEROES: Hero[] = [
+    {id:11, name:'John'},
+    {id:12, name:'Jerry'},
+    {id:13, name:'Alice'},
+    {id:14, name:'Adam'},
+    {id:15, name:'Pierre'},
+    {id:16, name:'Cindy'},
+    {id:17, name:'Jose'},
+    {id:18, name:'Dahlia'},
+    {id:19, name:'Barry'},
+    {id:20, name:'Larry'},
+]
